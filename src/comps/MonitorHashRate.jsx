b16615c1a7961fc4729206c25d50b0faa9bc7b19
xiepeerless <peerelss@163.com>
2023-02-17 03:55:40 -0800

    add timer

--- src/comps/MonitorHashRate.jsx
+++ src/comps/MonitorHashRate.jsx
@@ -27,29 +27,47 @@ const line_list = [
   "line_o_3",
   "line_o_4",
 ];
-const data = [
-  { name: "1月", uv: 4000, pv: 2400, amt: 2400 },
-  { name: "2月", uv: 3000, pv: 1398, amt: 2210 },
-  { name: "3月", uv: 2000, pv: 9800, amt: 2290 },
-  { name: "4月", uv: 2780, pv: 3908, amt: 2000 },
-  { name: "5月", uv: 1890, pv: 4800, amt: 2181 },
-  { name: "6月", uv: 2390, pv: 3800, amt: 2500 },
-  { name: "7月", uv: 3490, pv: 4300, amt: 2100 },
-  { name: "8月", uv: 3490, pv: 4300, amt: 2100 },
-  { name: "9月", uv: 3490, pv: 4300, amt: 2100 },
-  { name: "10月", uv: 3490, pv: 4300, amt: 2100 },
-  { name: "11月", uv: 3490, pv: 4300, amt: 2100 },
-  { name: "12月", uv: 3490, pv: 4300, amt: 2100 },
-];
+const data_2_monitor = (data) => {
+  return {
+    name: data.name,
+    line_j: data.line_j - 600,
+    line_e1: data.line_e1,
+    line_e2: data.line_e2,
+    line_i_k: data.line_i_k - 600,
+    line_e_i_m: data.line_e_i_m - 800,
+    line_e: data.line_e,
+    line_i: data.line_i,
+    line_k_1: data.line_k_1 - 300,
+    line_k_2: data.line_k_2,
+    line_k_3: data.line_k_3 - 100,
+    line_k_4: data.line_k_4 - 100,
+    line_o_1: data.line_o_1 - 200,
+    line_o_2: data.line_o_2 - 100,
+    line_o_3: data.line_o_3,
+    line_o_4: data.line_o_4 - 100,
+  };
+};
 export default function MonitorHashRate() {
   const [lineData, setLineData] = useState();
+  const [timeId, setTimeId] = useState(null);
+  useEffect(() => {
+    const id = setInterval(() => {
+      updateLineData();
+      console.log(id);
+    }, 15 * 1000);
+    setTimeId(id);
+    return () => {
+      clearInterval(timeId);
+    };
+  }, []);
+
   const updateLineData = async () => {
     const result = await axios(
       "http://127.0.0.1:5050/get_all_line_hash_rate_monitor"
     );
-    console.log(result.data);
+    const data_new = result.data.result.map(data_2_monitor);
 
-    setLineData(result.data.result);
+    setLineData(data_new);
   };
   useEffect(() => {
     updateLineData();
@@ -57,7 +75,7 @@ export default function MonitorHashRate() {
   return (
     <div>
       <LineChart
-        width={1200}
+        width={800}
         height={700}
         data={lineData}
         margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
@@ -70,23 +88,23 @@ export default function MonitorHashRate() {
         <Line
           type="monotone"
           dataKey="line_j"
-          stroke="#f7150e"
+          stroke="red"
           activeDot={{ r: 8 }}
         />
-        <Line type="monotone" dataKey="line_e1" stroke="#fca709" />
-        <Line type="monotone" dataKey="line_e2" stroke="#28ec0e" />
-        <Line type="monotone" dataKey="line_i_k" stroke="#82ca9d" />
-        <Line type="monotone" dataKey="line_e_i_m" stroke="#13d0e9" />
-        <Line type="monotone" dataKey="line_e" stroke="#086ee2" />
-        <Line type="monotone" dataKey="line_i" stroke="#cc1686" />
-        <Line type="monotone" dataKey="line_k_1" stroke="#caa882" />
-        <Line type="monotone" dataKey="line_k_2" stroke="#3e16cc" />
-        <Line type="monotone" dataKey="line_k_3" stroke="#042538" />
-        <Line type="monotone" dataKey="line_k_4" stroke="#3e16cc" />
-        <Line type="monotone" dataKey="line_o_1" stroke="#82ca9d" />
-        <Line type="monotone" dataKey="line_o_2" stroke="#3e16cc" />
-        <Line type="monotone" dataKey="line_o_3" stroke="#82ca9d" />
-        <Line type="monotone" dataKey="line_o_4" stroke="#3e16cc" />
+        <Line type="monotone" dataKey="line_e1" stroke="red" />
+        <Line type="monotone" dataKey="line_e2" stroke="red" />
+        <Line type="monotone" dataKey="line_i_k" stroke="red" />
+        <Line type="monotone" dataKey="line_e_i_m" stroke="black" />
+        <Line type="monotone" dataKey="line_e" stroke="black" />
+        <Line type="monotone" dataKey="line_i" stroke="black" />
+        <Line type="monotone" dataKey="line_k_1" stroke="black" />
+        <Line type="monotone" dataKey="line_k_2" stroke="black" />
+        <Line type="monotone" dataKey="line_k_3" stroke="black" />
+        <Line type="monotone" dataKey="line_k_4" stroke="green" />
+        <Line type="monotone" dataKey="line_o_1" stroke="green" />
+        <Line type="monotone" dataKey="line_o_2" stroke="green" />
+        <Line type="monotone" dataKey="line_o_3" stroke="green" />
+        <Line type="monotone" dataKey="line_o_4" stroke="green" />
       </LineChart>
     </div>
   );
